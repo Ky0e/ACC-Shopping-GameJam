13de3b88fcdb35ab4ea9745e42d58dba6ed9fc42
Bryan Hollingsworth <kwg@users.noreply.github.com>
2024-05-10 14:22:05 -0500

    Boss Spawning

--- Assets/Scripts/Environment/RoomEvent.cs
+++ Assets/Scripts/Environment/RoomEvent.cs
@@ -9,38 +9,7 @@ public enum RoomEventType
     Puzzle
 }
 
-public class RoomEvent : MonoBehaviour
+public interface RoomEvent
 {
-    [SerializeField] private RoomEventType roomEventType;
-    [SerializeField] private CardSO item;
-    [SerializeField] private List<DoorController> doors;
-
-    private void OnTriggerEnter(Collider _player)
-    {
-        if (_player.gameObject.CompareTag("Player"))
-        {
-            switch(roomEventType)
-            {
-                case RoomEventType.Item:
-                    if (!Player_Inventory.Instance.HasItem(item))
-                    {
-                        // Room event for item
-                    }
-                    break;
-                case RoomEventType.BossEnemy:
-                    // spawn enemy
-                    foreach (var door in doors)
-                    {
-                        door.CloseDoor();
-                        door.LockDoor();
-                    }
-                    break;
-                case RoomEventType.Puzzle:
-                    // start puzzle
-                    break;
-                default:
-                    break;
-            }
-        }
-    }
+    void OnTriggerEnter(Collider _player);
 }
